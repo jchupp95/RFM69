d0d59856e9c0d31e33a53251572375cebd8f3b28
jchupp <jchupp@Chupp_Computer>
2023-12-06 21:52:48 -0600

    Increased the size of Log() because of long debug strings in RFM69 code

--- RFM69/Core/Src/Debug.c
+++ RFM69/Core/Src/Debug.c
@@ -6,7 +6,7 @@
 extern UART_HandleTypeDef huart2;
 
 void Log(char *inputString){
-	char debugString[100];
+	char debugString[250];
 	int debugStringLen;
 	debugStringLen = sprintf(debugString,inputString);
 	HAL_UART_Transmit(&huart2, (uint8_t *)debugString, debugStringLen, 100);
